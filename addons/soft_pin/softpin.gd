7b60ec4a2c6a99de5684692c3ab31e9ae746f9c2
Cake-Holding Lady <138309951+Laporteusedegateaux@users.noreply.github.com>
2025-02-22 19:03:20 +0100

    Updated editor interface
    - Replaced the "start pinning" dropdown by an inspector button (4.4 required only for this)
    - Grey out the button if conditions are not met for the function to work
    - Removed the checks made redundants by the button's logic
    - Made it possible to chose the vertex color to pin down (black by default)
    - Slightly cleaned the @exports to use expor_enum instead of a locally defined enum
    - Removed the status variable that show up in the inspector
    - Removed a few unnecessary print()s

--- addons/soft_pin/softpin.gd
+++ addons/soft_pin/softpin.gd
@@ -2,100 +2,76 @@
 @tool
 extends Node
 
-enum PinState
-{
-	OFF = 0,
-	ON = 1,
-}
+## Variables 
+@export_enum("MeshInstance3D","Skeleton3D") var pin_type :
+	set(value):
+		pin_type = value
+		notify_property_list_changed()
+@export var softbody: SoftBody3D :
+	set(value):
+		softbody = value
+		notify_property_list_changed()
+@export var bone_attachment: BoneAttachment3D :
+	set(value) :
+		bone_attachment = value
+		notify_property_list_changed()
+@export_color_no_alpha var pinned_vertex_color
+@export_tool_button("Pin vertices") var pinning = assign_pins
 
-enum PinType
-{
-	MESHINSTANCE3D = 0,
-	SKELETON3D = 1,
-}	
-
-
-#Variables 
-@export var softbody: SoftBody3D
-@export var boneattachment: BoneAttachment3D
-@export var pin_type:PinType
-@export var start_pinning: PinState
-@export var pinning_status = "No softbody assigned"
 
 var mesh : Mesh
-var vert_amount 
-var white_color = Color(1,1,1,1)
-var black_color = Color(0,0,0,1)
+var vert_amount
 
 var meshdatatool = MeshDataTool.new()
 var arraymesh = ArrayMesh.new()
 var arrays = []
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+func _validate_property(property):
+	print(property["name"])
+	if property["name"] == "pinning" :
+		if softbody == null :
+			property.usage |= PROPERTY_USAGE_READ_ONLY
+		elif pin_type == 1 and !bone_attachment :
+			property.usage |= PROPERTY_USAGE_READ_ONLY
+		else :
+			property.usage |= PROPERTY_USAGE_EDITOR
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	if (Engine.is_editor_hint()):
-		if (start_pinning == PinState.ON):
-			assign_pins()  
-			start_pinning = 0
-	
 func assign_pins():
-	#Set the Mesh resource, Godot Collections Array, Array Mesh , and Mesh Data Tool
-		if (softbody != null):
-			mesh = softbody.mesh
-			arrays = mesh.surface_get_arrays(0)
-			arraymesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
-			meshdatatool.create_from_surface(arraymesh,0)
-			pinning_status = "pinning..."
-			
-			vert_amount = meshdatatool.get_vertex_count()
-			
-			if (pin_type == PinType.MESHINSTANCE3D):
-				if (boneattachment != null):
-					printerr("PINTYPE ERROR! SOFTPIN ACTION REQUIRED: SWITCH PINTYPE TO SKELETON3D WHEN USING BONE ATTACHMENT")
-					pinning_status = "PINTYPE ERROR! Switch to Skeleton3D"
-					return
-				for i in range(vert_amount):
-					var vertexcolor = meshdatatool.get_vertex_color(i)
-					if (vertexcolor != white_color and vertexcolor != black_color):
-						#COLOR DETECTED NO PIN
-						softbody.set_point_pinned(i,false)
-					else:
-						#NO COLOR DETECTED SET PIN
-						softbody.set_point_pinned(i,true) 
-			
-			if (pin_type == PinType.SKELETON3D):
-				if (boneattachment != null):
-					var bonepath = softbody.get_path_to(boneattachment)
-					for i in range(vert_amount):
-						var vertexcolor = meshdatatool.get_vertex_color(i)
-						if (vertexcolor != white_color and vertexcolor != black_color):
-							#COLOR DETECTED NO PIN
-							softbody.set_point_pinned(i,false)
-						else:
-							#NO COLOR DETECTED SET PIN
-							softbody.set_point_pinned(i,true,bonepath) 
-					if(softbody.skeleton != null):
-						softbody.skeleton = ""
-				else: 
-					printerr("NULL REF! SOFTPIN ACTION REQUIRE: ASSIGN BONEATTACHMENT3D")
-					pinning_status = "NULL REF! Assign BoneAttachment" 
-					return
-				
-				#Clear
-			
-			print("SOFTPIN: PINNING COMPLETE")
-			pinning_status = softbody.name + " Pinning complete"
-			arraymesh.clear_surfaces()
-			meshdatatool.clear()
-			softbody = null
-			boneattachment = null
-			
-		else: 
-			printerr("NULL REF! SOFTPIN ACTION REQUIRE: ASSIGN SOFTBODY3D")
-			pinning_status = "NULL REF! Assign Softbody"
+	## Set the Mesh resource, Godot Collections Array, Array Mesh , and Mesh Data Tool
+	mesh = softbody.mesh
+	arrays = mesh.surface_get_arrays(0)
+	arraymesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	meshdatatool.create_from_surface(arraymesh,0)
+	print("SOFTPIN: Pinning...")
+	
+	vert_amount = meshdatatool.get_vertex_count()
+	
+	if (pin_type == 0):
+		for i in range(vert_amount):
+			var vertexcolor = meshdatatool.get_vertex_color(i)
+			if (vertexcolor != pinned_vertex_color):
+				## Color detected, don't pin.
+				softbody.set_point_pinned(i,false)
+			else:
+				## No color detected, set pinned point.
+				softbody.set_point_pinned(i,true) 
+	if (pin_type == 1):
+		var bonepath = softbody.get_path_to(bone_attachment)
+		for i in range(vert_amount):
+			var vertexcolor = meshdatatool.get_vertex_color(i)
+			if (vertexcolor != pinned_vertex_color):
+				## Color detected, don't pin.
+				softbody.set_point_pinned(i,false)
+			else:
+				## No color detected, set pinned point. 
+				softbody.set_point_pinned(i,true,bonepath) 
+		if(softbody.skeleton != null):
+			softbody.skeleton = ""
+	
+	print("SOFTPIN: Pinning completed without errors.")
+	arraymesh.clear_surfaces()
+	meshdatatool.clear()
+	softbody = null
+	bone_attachment = null
